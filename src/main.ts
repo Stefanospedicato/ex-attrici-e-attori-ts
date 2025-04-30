7d74791db64dbf8fef3739afce3694320d606ff2
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-04-30 11:02:37 +0200

    feat: milestone 2 completed

--- src/main.ts
+++ src/main.ts
@@ -19,3 +19,21 @@ type Person = {
   image: string
 }
 
+/*
+Crea un type alias Actress che oltre a tutte le proprietà di Person, aggiunge le seguenti proprietà:
+
+most_famous_movies: una tuple di 3 stringhe
+awards: una stringa
+nationality: una stringa tra un insieme definito di valori.
+Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, French, Indian, Israeli, Spanish, South Korean, Chinese.
+*/
+
+const nationalities = [
+  'American', 'British', 'Australian', 'Israeli-American', 'South African', 'French', 'Indian', 'Israeli', 'Spanish', 'South Korean', 'Chinese'
+];
+
+type Actress = Person & {
+  most_famous_movies: [string, string, string],
+  awards: string,
+  nationality: typeof nationalities[number]
+}
