41046bc60aed7457b16842f9b98661c456c78905
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-04-30 11:40:36 +0200

    feat: milestone 4 completed

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,3 @@
-/*
-Crea un type alias Person per rappresentare una persona generica.
-
-Il tipo deve includere le seguenti proprietà:
-
-id: numero identificativo, non modificabile
-name: nome completo, stringa non modificabile
-birth_year: anno di nascita, numero
-death_year: anno di morte, numero opzionale
-biography: breve biografia, stringa
-image: URL dell'immagine, stringa
-*/
-
 type Person = {
   name: string,
   birth_year: number,
@@ -18,15 +5,6 @@ type Person = {
   biography: string,
   image: string
 }
-
-/*
-Crea un type alias Actress che oltre a tutte le proprietà di Person, aggiunge le seguenti proprietà:
-
-most_famous_movies: una tuple di 3 stringhe
-awards: una stringa
-nationality: una stringa tra un insieme definito di valori.
-Le nazionalità accettate sono: American, British, Australian, Israeli-American, South African, French, Indian, Israeli, Spanish, South Korean, Chinese.
-*/
 
 const nationalities = [
   'American', 'British', 'Australian', 'Israeli-American', 'South African', 'French', 'Indian', 'Israeli', 'Spanish', 'South Korean', 'Chinese'
@@ -38,14 +16,6 @@ type Actress = Person & {
   nationality: typeof nationalities[number]
 }
 
-/*
-Crea una funzione getActress che, dato un id, effettua una chiamata a:
-
-GET https://boolean-spec-frontend.vercel.app/freetestapi/actresses/:id
-La funzione deve restituire l’oggetto Actress, se esiste, oppure null se non trovato.
-
-Utilizza un type guard chiamato isActress per assicurarti che la struttura del dato ricevuto sia corretta.
-*/
 function isActress(dati: unknown): dati is Actress {
   if (
     dati &&
@@ -92,4 +62,20 @@ async function getActress(id: number): Promise<Actress | null> {
   }
 }
 
-console.log(getActress(2));
+async function getAllActresses(): Promise<Actress[] | null> {
+  try {
+    const response = await fetch('https://boolean-spec-frontend.vercel.app/freetestapi/actresses');
+    if (!response.ok) {
+      throw new Error('Errore Http');
+    }
+    const dati: unknown = await response.json();
+    if (Array.isArray(dati) && dati.every(isActress)) {
+      return dati;
+    }
+    return null;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
